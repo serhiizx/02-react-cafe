307be34006a6ccf7c5806a9266d313a017c50c3c
Serhii Zhdaniuk <jdanuk@gmail.com>
2026-06-28 18:01:26 +0300

    feat: add sourcemap.

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,7 @@ import babel from "@rolldown/plugin-babel";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), babel({ presets: [reactCompilerPreset()] })],
+  build: {
+    sourcemap: true,
+  },
 });
